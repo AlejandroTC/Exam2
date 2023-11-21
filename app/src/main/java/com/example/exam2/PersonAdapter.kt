9fc6440a2b775ac5c3d5ba1ff7ef160a30444f0c
AlejandroTC <aletamayo-02@hotmail.com>
2023-11-20 19:45:46 -0600

    Finally

--- app/src/main/java/com/example/exam2/PersonAdapter.kt
+++ app/src/main/java/com/example/exam2/PersonAdapter.kt
@@ -6,7 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import androidx.room.Room
+import com.example.exam2.room.AppDatabase
+import com.example.exam2.room.Estado
 import com.example.exam2.room.Persona
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class PersonAdapter (private val listadoPersona: List<Persona>):RecyclerView.Adapter<PersonAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PersonAdapter.ViewHolder {
@@ -15,8 +22,17 @@ class PersonAdapter (private val listadoPersona: List<Persona>):RecyclerView.Ada
     }
 
     override fun onBindViewHolder(holder: PersonAdapter.ViewHolder, position: Int) {
+        val db = Room.databaseBuilder(holder.itemView.context, AppDatabase::class.java, "roomDB").build()
+
         holder.nombrePersona.text = listadoPersona[position].nombre
         holder.edadPersona.text = listadoPersona[position].edad.toString()
+        CoroutineScope(Dispatchers.Main).launch {
+            val estado: Estado = withContext(Dispatchers.IO) {
+                db.estadoDao().getEstadoById(listadoPersona[position].stateId)
+            }
+
+            holder.estadoPersona.text = estado.nombre
+        }
         holder.itemView.setOnClickListener{
             val intentActualizar = Intent(holder.itemView.context, UpdatePerson::class.java)
             intentActualizar.putExtra("id", listadoPersona[position].personId)
@@ -33,6 +49,7 @@ class PersonAdapter (private val listadoPersona: List<Persona>):RecyclerView.Ada
     inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
         val nombrePersona: TextView = itemView.findViewById(R.id.name_tv)
         val edadPersona: TextView = itemView.findViewById(R.id.age_tv)
+        val estadoPersona: TextView = itemView.findViewById(R.id.stateP)
     }
 
 }
